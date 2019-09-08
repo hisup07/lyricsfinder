f717ba9df6da498bf2eb9b351143a57752c08f8e
Supriyo Mondal <hisupriyooo@gmail.com>
2019-09-08 12:04:47 +0530

    Updates

---
+++ precache-manifest.5b0575ac4c99d27cac3696829b68cdd3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e69d52f58a00b7eec8fcc5b8759cfcb5",
+    "url": "/lyricsfinder/index.html"
+  },
+  {
+    "revision": "81d5178aee2359b8e87c",
+    "url": "/lyricsfinder/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "9892c0918c4726d2a283",
+    "url": "/lyricsfinder/static/js/2.2495a5d7.chunk.js"
+  },
+  {
+    "revision": "81d5178aee2359b8e87c",
+    "url": "/lyricsfinder/static/js/main.083f1c09.chunk.js"
+  },
+  {
+    "revision": "0aab52e1ac0b06d07308",
+    "url": "/lyricsfinder/static/js/runtime~main.60b50438.js"
+  }
+]);
